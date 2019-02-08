68dc2d4794977416b677c3dc30bee0134546de66
sharath Kumar <sharath.dt@gmail.com>
2019-02-08 19:59:24 +0530

    Add sales new style
    - fix product height

---
+++ _pages/sales-old.ml
@@ -0,0 +1,78 @@
+---
+layout: default
+title: Sales
+permalink: /sales/
+---
+
+<div id="wj-sales" class="py-lg">
+  <div class="container">
+    <div class="row animate-box">
+      <div class="col-md-6 col-md-offset-3 text-center wj-heading">
+        <h2>Antwort-Formular</h2>
+        <p>Faxen Sie uns unter: 0 76 64/93 00 - 29</p>
+        <p><small>Aktion gültig bis 28.02.2019</small></p>
+      </div>
+    </div>
+
+    <div class="row animate-box pb-md">
+      <div class="col-md-4">
+        <img src="/images/sales.jpg" alt="">
+      </div>
+      <div class="col-md-8">
+        <h2>Great White® - Kronentrenner-Set</h2>
+        <h4>mit GWZ (für Zirkonoxid) und GW2 (für Metall)</h4>
+        <ul>
+          <li>5 Great White® Z, Kugel, zur Trepanation von Zirkonoxid</li>
+          <li>5 Great White® Z, Konus, zum Auftrennen von Zirkonoxid </li>
+          <li>10 Great White® Gold, Zylinder, zum Auftrennen von Metall im bronzefarbenem Metall-Dispenser, sterilisierbar</li>
+        </ul>
+
+        <span class="text-warning">Normalpreis: € 156,40</span><br>
+        <span class="text-success">Angebotspreis: € 125,-</span>
+      </div>
+    </div>
+
+  <div class="row animate-box mt-md">
+    <div class="col-md-12">
+      <p class="text-center"><strong>Ja, bitte senden Sie mir ein Kronentrenner-Set GWZ + GW2</strong></p>
+    </div>
+</div>
+
+  <div class="row animate-box">
+    <div class="col-md-12">
+      <div class="alert alert-success text-center">Great White® Z (für Zirkonoxid) und Great White® Gold(für Metall) - Kronentrenner mit 10% Rabatt</div>
+    </div>
+</div>
+
+  <div class="row animate-box">
+      <div class="col-md-4"><strong>Item No.</strong></div>
+      <div class="col-md-6"><strong>Produktbezeichnung</strong></div>
+      <div class="col-md-2 pb-md"><strong>Anzahl Päckchen</strong></div>
+
+      <div class="col-md-4">18161</div>
+      <div class="col-md-6">GW® Z Konus, Stirn rund, ISO Ø 018, FG normal</div>
+      <div class="col-md-2 pb-md">______________</div>
+
+      <div class="col-md-4">18162</div>
+      <div class="col-md-6">GW® Z Kugel, ISO Ø 014, FG normal</div>
+      <div class="col-md-2 pb-md">______________</div>
+
+      <div class="col-md-4">18163</div>
+      <div class="col-md-6">GW® Z Kugel, ISO Ø 018, FG normal</div>
+      <div class="col-md-2 pb-md">______________</div>
+
+      <div class="col-md-4">18164
+        <img src="/images/sales-1.jpg" alt="">
+      </div>
+      <div class="col-md-6">GW® Z Ei, ISO Ø 023, FG normal<br>Originalpreis pro Packung (5 Bohrer): € 41,- netto</div>
+      <div class="col-md-2 pb-md">______________</div>
+
+      <div class="col-md-4">18162-5
+      <img src="/images/sales-2.jpg" alt=""></div>
+      <div class="col-md-6">GW® G Zylinder, Stirn rund, ISO Ø 012, FG normal<br>Originalpreis pro Packung (5 Bohrer): € 23,- netto</div>
+      <div class="col-md-2 pb-md">______________</div>
+
+  </div>
+
+  </div>
+</div>
